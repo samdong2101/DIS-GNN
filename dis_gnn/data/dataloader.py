cea9a7ba402a879f87a32926a1d22c39619a791e
Sam Dong <112105146+samdong2101@users.noreply.github.com>
2025-11-25 22:08:19 -0500

    Update dataloader.py

--- dis_gnn/data/dataloader.py
+++ dis_gnn/data/dataloader.py
@@ -1,8 +1,12 @@
+import numpy as np
+import torch
+import pandas as pd
+
 class DataLoader:
-  def __init__(df, batch_size = 32):
+  def __init__(self, df, batch_size = 32):
     self.df = df
     self.batch_size = batch_size
-    
+
   def load_data(self, df):
     node_features = [i for i in df['node_feature']]
     edge_indices = [i for i in df['edge_index']]
@@ -17,10 +21,9 @@ class DataLoader:
       num_batches = int(np.round(len(feature_list)/batch_size)) - 1
       batched_node_features = []
       batch_index = 0
-      
+
       for batch in range(num_batches):
           try:
-  
               batched_node_feature = np.concatenate(feature_list[batch_index:batch_index + batch_size])
               batched_node_features.append(torch.tensor(batched_node_feature))
               batch_index = batch_index + batch_size
@@ -28,8 +31,9 @@ class DataLoader:
               batched_node_feature = np.concatenate(feature_list[batch_index:])
               batched_node_features.append(torch.tensor(batched_node_feature))
       return batched_node_features
+
   def batch_edge_features(self, feature_list, batch_size):
-      num_batches = int(np.round(len(feature_list)/batch_size)) - 1 
+      num_batches = int(np.round(len(feature_list)/batch_size)) - 1
       batched_edge_features = []
       batch_index = 0
       for batch in range(num_batches):
@@ -49,8 +53,8 @@ class DataLoader:
       batched_edge_indices = []
       batch_index = 0
       shifted_tensors_list = []
-      batch = 0 
-      
+      batch = 0
+
       for b in range(num_batches):
           count = 0
           shift_factor = 0
@@ -63,14 +67,27 @@ class DataLoader:
                   count = count + 1
               except:
                   pass
-          
+
           batch = batch + batch_size
           shifted_tensors_list.append(shifted_tensors)
-  
+
       for batch in shifted_tensors_list:
           concat_batch = torch.cat(batch,dim = 1)
           batched_edge_indices.append(concat_batch)
       return batched_edge_indices
+
+  def batch_node_indices(self, feature_list, batch_size):
+      num_batches = int(np.round(len(feature_list)/batch_size)) - 1
+      batched_node_indices = []
+      i = 0
+      ind = 0
+      for batch in range(num_batches):
+          b_zero = [np.zeros(i.shape[0]) for i in feature_list[ind:ind+batch_size]]
+          b = [zero + i for i,zero in enumerate(b_zero)]
+          batched_node_indices.append(b)
+          ind = ind + batch_size
+      return batched_node_indices
+
 
   def batch_labels(self, feature_list, batch_size):
       num_batches = int(np.round(len(feature_list)/batch_size)) - 1
@@ -83,16 +100,16 @@ class DataLoader:
           repeated_tensor = batched_tensor
           batched_target_values.append(repeated_tensor)
       return batched_target_values
-  
+
   def batch_data(self, batch_size, node_features, edge_indices, edge_features, labels):
     batched_node_features = self.batch_node_features(node_features, self.batch_size)
     batched_edge_indices = self.batch_edge_indices(edge_indices, self.batch_size, node_features)
     batched_edge_features = self.batch_edge_features(edge_features, self.batch_size)
+    batched_node_indices = self.batch_node_indices(node_features, self.batch_size)
     batched_labels = self.batch_labels(labels, self.batch_size)
-    return batched_node_features, batched_edge_indices, batched_edge_features, batched_labels
+    return batched_node_features, batched_edge_indices, batched_edge_features, batched_labels, batched_node_indices
 
-  def get_data():
+  def get_data(self):
     node_features, edge_indices, band_gaps, edge_features, labels, structures = self.load_data(self.df)
-    batched_node_features, batched_edge_indices, batched_edge_features, batched_labels = self.batch_data(self, batch_size, node_features, edge_indices, edge_features, labels)
-    return batched_node_features, batched_edge_indices, batched_edge_featuers, batched_labels
-
+    batched_node_features, batched_edge_indices, batched_edge_features, batched_labels, batched_node_indices = self.batch_data(self.batch_size, node_features, edge_indices, edge_features, labels)
+    return batched_node_features, batched_edge_indices, batched_edge_features, batched_labels, batched_node_indices
