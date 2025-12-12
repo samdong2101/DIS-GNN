6df1df17c7b9c3b08189475c11114548de1db086
Sam Dong <sam.dong@c0602a-s3.ufhpc>
2025-12-12 17:20:14 -0500

    update conv.py, integrate linear graphs, gated mlps, etc.

--- dis_gnn/model/conv.py
+++ dis_gnn/model/conv.py
@@ -10,7 +10,7 @@ import math
 import time
 from torch_scatter import scatter_mean
 from .layers import ResidualMessageMLP
-
+from .layers import GatedMLP
 class Convolution(MessagePassing):
     def __init__(self, in_dim, out_dim, n_resid_layers = 1, n_mlp_layers = 1, **kwargs):
         super(Convolution, self).__init__(node_dim=0, aggr='add', **kwargs)
@@ -21,13 +21,14 @@ class Convolution(MessagePassing):
         self.n_resid_layers = n_resid_layers
         self.n_mlp_layers = n_mlp_layers
         self.mlp_hidden = 64
-        self.bond_linears = ResidualMessageMLP(in_dim*3, out_dim, hidden_dim = self.resid_hidden,n_layers = self.n_resid_layers)
-        self.node_linears = ResidualMessageMLP(in_dim*2+1, out_dim,hidden_dim = self.resid_hidden, n_layers = self.n_resid_layers)
+        self.bond_linears = GatedMLP(in_dim*3, out_dim, hidden_dim = self.resid_hidden,n_layers = self.n_resid_layers) # used to be ResidualMessageMLP
+        self.node_linears = GatedMLP(in_dim*3+1, out_dim,hidden_dim = self.resid_hidden, n_layers = self.n_resid_layers) # used to be ResidualMessageMLP
         #self.drop = nn.Dropout(dropout)
+        self.norm = nn.LayerNorm(out_dim)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        print('n_mlp_layers:', n_mlp_layers) 
-    def forward(self, node_inp, edge_index, edge_feature, global_state):
-        concat_edge, concat_node = self.message(edge_index[0],edge_index[1], node_inp, node_inp, edge_feature, global_state)
+ 
+    def forward(self, node_inp, edge_index, edge_feature, global_state = None, cells = None, coords = None):
+        concat_edge, concat_node = self.message(edge_index[0],edge_index[1], node_inp, node_inp, edge_feature, global_state, cells, coords)
         return concat_node
 
     def average_edge_vector(self, edge_index, edge_attr):
@@ -38,28 +39,31 @@ class Convolution(MessagePassing):
         out = torch.where(torch.isnan(out), torch.zeros_like(out), out)
         return out
     
-    def message(self, edge_index_i, edge_index_j, node_inp_i, node_inp_j, edge_feature, global_state):
+    def message(self, edge_index_i, edge_index_j, node_inp_i, node_inp_j, edge_feature, global_state, cells, coords):
         '''
             j: source, i: target; <j, i>
         '''
         edge_index = [edge_index_i, edge_index_j]
         node_inp_i_rollout = node_inp_i[edge_index_i]
         node_inp_j_rollout = node_inp_j[edge_index_j]
+        cells = cells.reshape(cells.shape[0],1)
+        
         concat_edge = torch.concat([node_inp_i_rollout, node_inp_j_rollout, edge_feature],dim = 1)
         concat_edge = self.bond_linears.forward(concat_edge)
         
         agg_edges = self.average_edge_vector(edge_index, concat_edge)
         global_state = global_state.reshape(global_state.shape[0],1)
-        #print('node_inp_i shape:', node_inp_i.shape)
-        #print('agg_edges shape:', agg_edges.shape)
-        #print('global_state shape:', global_state.shape)
-        #global_state_exp = global_state.expand(node_inp_i.shape[0], 0)
-        #print('global_state_exp:', global_state_exp)
-        #print(global_state)
-        concat_node = torch.concat([node_inp_i, agg_edges, global_state],dim = 1)
-        #print('concat_node shape:', concat_node.shape)
-        concat_node = self.node_linears.forward(concat_node)
         
+        try:
+            concat_node = torch.concat([node_inp_i, agg_edges, coords, global_state],dim = 1)
+        except:
+            print('edge_index:', edge_index)
+            print('node_inp_i shape:', node_inp_i.shape)
+            print('agg_edges shape:', agg_edges.shape)
+            print('coords shape:', coords.shape)
+            print('global_state shape:', global_state.shape)
+        concat_node = self.node_linears.forward(concat_node)
+        concat_node = self.norm(concat_node) 
         return concat_edge, concat_node 
                 
    
@@ -67,5 +71,5 @@ class GeneralConv(nn.Module):
     def __init__(self, in_hid, out_hid, n_resid_layers = 1, n_mlp_layers = 1):
         super(GeneralConv, self).__init__()
         self.base_conv = Convolution(in_hid, out_hid, n_resid_layers, n_mlp_layers)
-    def forward(self, meta_xs, edge_index, edge_feature, global_state):
-        return self.base_conv(meta_xs, edge_index, edge_feature, global_state)
+    def forward(self, meta_xs, edge_index, edge_feature, global_state, cells, coords):
+        return self.base_conv(meta_xs, edge_index, edge_feature, global_state, cells, coords)
