3b8dd05ff3cff8ce9fcff0ee4972f30ac6b8dc9e
Sam Dong <112105146+samdong2101@users.noreply.github.com>
2026-06-09 14:19:00 -0400

    Create gnn_clean.py

---
+++ dis_gnn/model/gnn_clean.py
@@ -0,0 +1,230 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from .layers import mlp
+from .layers import GatedMLP
+from .conv import GeneralConv
+
+class GNN(nn.Module):
+    def __init__(self, 
+                 in_dim, 
+                 line_in_dim, 
+                 n_hid, 
+                 n_layers, 
+                 batch_size, 
+                 dropout = 0.2, 
+                 ff_hidden = 32, 
+                 n_resid_layers = 1, 
+                 n_mlp_layers = 1, 
+                 include_coords = False, 
+                 include_cells = False, 
+                 include_states = False):
+        super(GNN, self).__init__()
+        self.gcs = nn.ModuleList()
+        self.line_gcs = nn.ModuleList()
+        self.final_gcs = nn.ModuleList()
+        self.in_dim    = in_dim
+        self.line_in_dim = line_in_dim
+        self.n_hid     = n_hid
+        self.node_ff  = nn.ModuleList()
+        self.edge_ff = nn.ModuleList()
+        self.drop      = nn.Dropout(dropout)
+        
+
+        self.include_coords = include_coords
+        self.include_cells = include_cells
+        self.include_states = include_states
+        self.node_linear = nn.Linear(in_dim,n_hid)
+        self.line_node_linear = nn.Linear(line_in_dim,n_hid)
+        self.edge_linear = nn.Linear(40, n_hid) # --> changed to 40 from 80
+        self.line_edge_linear = nn.Linear(40, n_hid)
+        self.state_linear = nn.Linear(batch_size, batch_size)
+        self.cell_linear = nn.Linear(batch_size*9, batch_size)
+        self.coord_linear = nn.Linear(3, n_hid)
+
+        self.norm = nn.LayerNorm(n_hid)
+        self.node_ff = mlp(in_dim, 
+                           n_hid, 
+                           hidden_dim=ff_hidden, 
+                           num_layers = n_mlp_layers)
+        self.line_node_ff = mlp(line_in_dim, 
+                                n_hid, 
+                                hidden_dim=ff_hidden,
+                                num_layers=n_mlp_layers)
+        self.edge_ff = mlp(40, 
+                           n_hid, 
+                           hidden_dim=ff_hidden, 
+                           num_layers = n_mlp_layers) # --> changed to 40 from 80 
+        self.line_edge_ff = mlp(40, 
+                                n_hid, 
+                                hidden_dim=ff_hidden,
+                                num_layers = n_mlp_layers) 
+        self.state_ff = mlp(batch_size, 
+                            batch_size, 
+                            hidden_dim = ff_hidden, 
+                            num_layers = n_mlp_layers) 
+        self.cell_ff = mlp(batch_size*9, 
+                           batch_size, 
+                           hidden_dim = ff_hidden, 
+                           num_layers = n_mlp_layers)
+        self.coord_ff = mlp(3, 
+                            n_hid, 
+                            hidden_dim = ff_hidden, 
+                            num_layers = n_mlp_layers)
+        self.alpha = nn.Parameter(torch.ones(n_hid))
+        for l in range(n_layers - 1):   
+            self.gcs.append(GeneralConv(n_hid, 
+                                        n_hid, 
+                                        n_resid_layers = n_resid_layers, 
+                                        n_mlp_layers = n_mlp_layers, 
+                                        include_coords = self.include_coords, 
+                                        include_cells = self.include_cells, 
+                                        include_states = self.include_states))
+            self.line_gcs.append(GeneralConv(n_hid, 
+                                             n_hid, 
+                                             n_resid_layers = n_resid_layers, 
+                                             n_mlp_layers = n_mlp_layers, 
+                                             include_coords = self.include_coords, 
+                                             include_cells = self.include_cells, 
+                                             include_states = self.include_states))
+            self.final_gcs.append(GeneralConv(n_hid, 
+                                              n_hid, 
+                                              n_resid_layers = n_resid_layers, 
+                                              n_mlp_layers = n_mlp_layers, 
+                                              include_coords = self.include_coords, 
+                                              include_cells = self.include_cells, 
+                                              include_states = self.include_states))
+        self.gcs.append(GeneralConv(n_hid, 
+                                    n_hid, 
+                                    n_resid_layers = n_resid_layers, 
+                                    n_mlp_layers = n_mlp_layers, 
+                                    include_coords = self.include_coords, 
+                                    include_cells = self.include_cells, 
+                                    include_states = self.include_states))
+        self.line_gcs.append(GeneralConv(n_hid, 
+                                         n_hid, 
+                                         n_resid_layers=n_resid_layers, 
+                                         n_mlp_layers=n_mlp_layers, 
+                                         include_coords = self.include_coords, 
+                                         include_cells = self.include_cells, 
+                                         include_states = self.include_states))
+        self.final_gcs.append(GeneralConv(n_hid, 
+                                          n_hid, 
+                                          n_resid_layers=n_resid_layers,
+                                          n_mlp_layers=n_mlp_layers,
+                                          include_coords = self.include_coords, 
+                                          include_cells = self.include_cells, 
+                                          include_states = self.include_states))
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    def forward(self, 
+                node_feature, 
+                edge_index,  
+                edge_feature, 
+                line_node_feature = None, 
+                line_edge_index = None, 
+                line_edge_feature = None, 
+                global_state = None,
+                group_size = None, 
+                cell = None, 
+                coords = None): 
+        
+        node_res = self.node_ff(node_feature)
+        meta_node = self.residual_connection(node_feature, self.node_linear, self.node_ff) 
+        meta_edge = self.residual_connection(edge_feature, self.edge_linear, self.edge_ff)
+         
+        if None not in (line_node_feature, line_edge_index, line_node_feature):
+            #line_node_res = self.line_node_ff(line_node_feature)
+            line_meta_node = self.residual_connection(line_node_feature, self.line_node_linear, self.line_node_ff)
+            line_meta_edge = self.residual_connection(line_edge_feature, self.line_edge_linear, self.line_edge_ff)
+            
+
+        if global_state is not None:
+            meta_state = self.residual_connection(global_state, self.state_linear, self.state_ff)
+            meta_state = torch.repeat_interleave(meta_state, group_size).to(self.device) 
+        else:
+            meta_state = None
+
+        if cell is not None:
+            meta_cell = self.residual_connection(cell, self.cell_linear, self.cell_ff) 
+            meta_cell = torch.repeat_interleave(meta_cell, group_size).to(self.device)
+        else:
+            meta_cell = None
+
+        if coords is not None:
+            meta_coords = self.residual_connection(coords, self.coord_linear, self.coord_ff) 
+        else:
+            meta_coords = None
+
+        for i in range(len(self.gcs)):
+            gc = self.gcs[i]
+            lgc = self.line_gcs[i]
+            fgc = self.final_gcs[i]
+            if i == 0:
+                meta_node, meta_edge = gc(meta_node, 
+                                          edge_index, 
+                                          meta_edge, 
+                                          meta_state, 
+                                          meta_cell, 
+                                          meta_coords, 
+                                          'crystal')
+                line_meta_node, line_meta_edge = lgc(meta_edge, 
+                                                     line_edge_index, 
+                                                     line_meta_edge, 
+                                                     None, 
+                                                     None, 
+                                                     None, 
+                                                     'line')
+                final_node, final_edge = fgc(meta_node, 
+                                             edge_index, 
+                                             line_meta_node, 
+                                             meta_state, 
+                                             meta_cell, 
+                                             meta_coords,
+                                             'crystal')
+            else:
+
+                meta_node, meta_edge = gc(meta_node, 
+                                          edge_index, 
+                                          meta_edge, 
+                                          meta_state, 
+                                          meta_cell, 
+                                          meta_coords, 
+                                          'crystal')
+                line_meta_node, line_meta_edge = lgc(line_meta_node, 
+                                                     line_edge_index, 
+                                                     line_meta_edge, 
+                                                     None, 
+                                                     None, 
+                                                     None, 
+                                                     'line')
+                final_node, final_edge = fgc(meta_node, 
+                                             edge_index, 
+                                             line_meta_node, 
+                                             meta_state, 
+                                             meta_cell, 
+                                             meta_coords,
+                                             'crystal')
+            alpha = F.relu(self.alpha) 
+            final_node =  alpha * node_res + final_node #meta_node
+        return final_node
+
+    def pool(self, 
+             atom_features, 
+             idx):
+        assert sum([len(index) for index in idx]) == atom_features.shape[0]
+        agg_feature = [torch.mean(atom_features[index], dim = 0, keepdim = True) for index in idx]
+        return torch.cat(agg_feature,dim = 0)
+
+    def residual_connection(self, 
+                            feature, 
+                            lin_layer, 
+                            ff):
+        
+        feature_res = lin_layer(feature) 
+        feature_skip = ff(feature)
+        tot_feature = feature_res + feature_skip 
+        dropped_feature = self.drop(tot_feature)
+        
+        return dropped_feature 
+
